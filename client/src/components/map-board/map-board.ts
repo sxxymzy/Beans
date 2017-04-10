cfa1d1af11b1f1b0c66ac6ab6928c1c46c91ae13
max <max.meng@appartner.cn>
2017-04-10 21:26:12 +0800

    add bg

--- client/src/components/map-board/map-board.ts
+++ client/src/components/map-board/map-board.ts
@@ -13,23 +13,57 @@ import { Component } from '@angular/core';
 export class MapBoard {
 
   game: Phaser.Game;
+  cursors: Phaser.CursorKeys;
+  bgTileSprite: Phaser.TileSprite;
+  icoMe: Phaser.Sprite;
   constructor() {
     this.game = new Phaser.Game(
       "100", "100",
       Phaser.AUTO,
       'game-board',
-      { preload: this.preload, create: this.create },
-      true);
+      { preload: this.preload, create: this.create, update: this.update, render: this.render },
+      false);
   }
 
-
   preload() {
-    this.game.load.image('logo', 'assets/sprites/me.ico');
+    this.game.load.image("bgTileImage", "assets/tile-grid.png");
+    this.game.load.image("icoMe", "assets/sprites/me.ico");
   }
 
   create() {
-    var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+    let icoMeW = 64;
+    let tileW = 44;
+    this.bgTileSprite = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, "bgTileImage");
+    let scalMe = this.game.width * 0.1 / icoMeW;
+    let offsetMe = icoMeW / 2 * scalMe;
+    this.icoMe = this.game.add.sprite(this.game.width / 2 - offsetMe, this.game.height / 2 - offsetMe, "icoMe");
+
+    this.icoMe.scale.x = scalMe;
+    this.icoMe.scale.y = scalMe;
+    console.log(scalMe, offsetMe);
+
+    let scaleBg = this.game.width / 15 / tileW; // 15 grid each row, 44 is tile image width
+    this.bgTileSprite.tileScale.x = scaleBg;
+    this.bgTileSprite.tileScale.y = scaleBg;
+
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+  }
+  update() {
+    if (this.cursors.left.isDown) {
+      this.bgTileSprite.tilePosition.x += 8;
+    }
+    else if (this.cursors.right.isDown) {
+      this.bgTileSprite.tilePosition.x -= 8;
+    }
+
+    if (this.cursors.up.isDown) {
+      this.bgTileSprite.tilePosition.y += 8;
+    }
+    else if (this.cursors.down.isDown) {
+      this.bgTileSprite.tilePosition.y -= 8;
+    }
+  }
+  render() {
   }
 
 }
--- client/src/providers/geo-location.ts
+++ client/src/providers/geo-location.ts
@@ -3,19 +3,16 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 /*
-  Generated class for the Spot provider.
+  Generated class for the GeoLocation provider.
 
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
   for more info on providers and Angular 2 DI.
 */
 @Injectable()
-export class Spot {
+export class GeoLocation {
 
   constructor(public http: Http) {
-    console.log('Hello Spot Provider');
-  }
-  saveSpot(lat: string, long: string) {
-
+    console.log('Hello GeoLocation Provider');
   }
 
 }
